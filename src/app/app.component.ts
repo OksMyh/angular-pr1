9d5f0958390d27d8ecc3cf64097daa9f1b5599bf
Oksana <oks.mykhailiuk@gmail.com>
2019-01-27 14:11:33 +0200

    angular-error-bad practik

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,18 @@ export class AppComponent {
   show = true;
   login = false;
   register = true;
+  notFiled = (form) => {
+    console.log(form);
+    const requiredFiled = document.createElement('div');
+    if (!this.show) {
+      console.log(form.value);
+      requiredFiled.innerText = `${form.value}*blank field fill out`;
+      document.body.appendChild(requiredFiled);
+      requiredFiled.style.color = 'red';
+    } else {
+      requiredFiled.style.display = 'none';
+    }
+  }
   onClickLogin = () => {
     this.show = false;
     this.login = true;
@@ -19,11 +31,10 @@ export class AppComponent {
     this.login = false;
     this.register = true;
   }
-
   sendForm = (info) => {
+    console.log(info);
     console.log(info.value);
     this.show = true;
   }
-
 }
 
